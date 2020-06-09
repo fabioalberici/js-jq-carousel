6874bccb885241d194f603484a1ac83fd8e6c04f
fabioalberici <fabioalbe79@gmail.com>
2020-06-09 14:14:31 +0200

    ultimate funzione di 'scorrimento'

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $(document).ready(
 //quando facciamo next scorre in avanti immagine e pallino
  $('.next').click(
    function() {
-     alert('ok')
+     immagineDopo();
    }
  )
 //quando facciamo prev  scorre all'indietro immagine e pallino
@@ -14,3 +14,49 @@ $(document).ready(
   )
  }
 )
+
+
+//funzione per scorrere le immagini in avanti
+function immagineDopo(){
+  var immagine = $('.images img.active');
+  var pallino = $('.nav i.active');
+
+  immagine.removeClass('active');
+  pallino.removeClass('active');
+
+// ora sposto le immagini, specificando il caso
+// in cui l'elemento selezionato sia l'ultimo
+  if (immagine.hasClass('last')) {
+    $('.images img.first').addClass('active');
+    $('.nav i.first').addClass('active');
+  }
+  //se invece non si tratta dell'ultimo
+  else {
+    immagine.next('img').addClass('active');
+    pallino.next('i').addClass('active');
+  }
+}
+
+
+//funzione per scorrere le immagini all'indietro
+
+function immaginePrima() {
+  var immagine = $('.images img.active');
+  var pallino = $('.nav i.active');
+
+  immagine.removeClass('active');
+  pallino.removeClass('active');
+
+//ora sposto le immagini, specificando il caso in cui
+//l'elemento selezionato sia il primo, e io voglia andare indietro
+
+  if (immagine.hasClass('first')) {
+    $('.images img.last').addClass('active');
+    $('.nav i.last').addClass('active');
+  }
+// se invece si tratta del primo
+  else {
+    immagine.prev('img').addClass('active');
+    pallino.prev('i').addClass('active');
+  }
+}
